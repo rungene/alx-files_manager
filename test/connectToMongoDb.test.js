4c84ff8d62c2216018e1ebb65ec3d0e8a4932460
rungene <rungenesamuel@gmail.com>
2024-09-13 13:01:25 +0300

    Testing connection to MongoDB

--- test/connectToMongoDb.test.js
+++ test/connectToMongoDb.test.js
@@ -1,20 +1,21 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const mongodb = require('mongodb');
-const dbClient = require('../utils/db');
 
 describe('DBClient', function() {
   afterEach(function() {
     // Automatically restore all stubs after each test
     sinon.restore();
+    delete require.cache[require.resolve('../utils/db')]
   });
 
   it('should initialize and connect successfully', function(done) {
     const connectStub = sinon.stub(mongodb.MongoClient.prototype, 'connect').callsFake(function(cb) {
       cb(null); // No Error
     });
-    
     // Wait for the next tick to allow the callback to execute
+    const dbClient = require('../utils/db');
+
     process.nextTick(() => {
       expect(dbClient.isAlive()).to.be.true;
       expect(connectStub.calledOnce).to.be.true;
@@ -28,9 +29,11 @@ describe('DBClient', function() {
       cb(new Error('Failed to connect')); 
     });
 
+    const dbClient = require('../utils/db');
+
     process.nextTick(() => {
       expect(dbClient.isAlive()).to.be.false;
-      expect(connectStub.calledOnce).to.be.true();
+      expect(connectStub.calledOnce).to.be.true;
       done();
     });
   });
